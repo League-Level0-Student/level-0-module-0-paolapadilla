2b1a8fa020ac573d0e4b3edeff8cc773c85ee9b4
DemoliciousDt <league@Thes-Mac-mini.local>
2017-08-19 11:29:30 -0700

    i learned how to do loops

--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -3,28 +3,32 @@ import org.jointheleague.graphical.robot.Robot;
 // This recipe draws a square using the Robot
 
 public class RobotSquare {
-    public static void main(String[] args) throws Exception {
- 
-    	// 1. Make a new Robot
-Robot r = new Robot();
-r.penDown();
-r.setSpeed(10);
-r.move(400);
+	public static void main(String[] args) throws Exception {
 
-        // 3. Put the robot's pen down
+		// 1. Make a new Robot
+		Robot r = new Robot();
+		r.penDown();
+		r.setSpeed(100);
 
+		// 3. Put the robot's pen down
 
-        // 6. Make the robot move as fast as possible
+		// 6. Make the robot move as fast as possible
 
+		// 5. Do everything below here 4 times
+		for (int i = 0; i < 4; i++) {
+			r.setPenWidth(200);
+			r.setRandomPenColor();
 
-        // 5. Do everything below here 4 times
-
-
-        //         2. Move your robot 200 pixels
-
-
-        //         4. Turn the robot 90 degrees to the right (90 degrees)
-
-
-    }
+			// 2. Move your robot 200 pixels
+			r.move(200);
+			// 4. Turn the robot 90 degrees to the right (90 degrees)
+			r.turn(90);
+		}
+		r.move(200);
+		r.turn(45);
+		r.move(150);
+		r.turn(90);
+		r.move(150);
+		r.sparkle();
+	}
 }
